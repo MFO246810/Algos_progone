9c09e6a6db8bb8dbfc8de9309d9472884e57f72f
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-02-10 15:52:33 -0600

    Merge Sort done testing begins's

--- arrsort.pas
+++ arrsort.pas
@@ -1,7 +1,7 @@
 program ArrSort;
 
 var
-    Test: array [0..4] of Integer;
+    Test: array [0..5] of Integer; result: Boolean;
 
 procedure BubbleSort(var arr: array of Integer; n: Integer);
     var
@@ -154,16 +154,99 @@ procedure QuickSortV2(var arr: array of Integer; low: Integer; high: Integer);
             end;
     end;
 
-{
-procedure MergeSort(var arr: array of Integer);
-    begin
 
+
+procedure Merge(var arr: array of Integer; i, j, m: integer);
+    var
+        tempArr: array of Integer;
+        L, R, k, mid: integer;
+    begin
+        mid := (i + j) div 2;
+        L := i;
+        R := mid + 1;
+        k := i;
+
+        SetLength(tempArr, j + 1); 
+        while (L <= mid) and (R <= j) do
+            begin
+                if arr[L] <= arr[R] then
+                    begin
+                        tempArr[k] := arr[L];
+                        L := L + 1;
+                    end
+                else
+                    begin
+                        tempArr[k] := arr[R];
+                        R := R + 1;
+                    end;
+                k := k + 1;
+            end;
+
+        while L <= mid do
+            begin
+                tempArr[k] := arr[L];
+                L := L + 1;
+                k := k + 1;
+            end;
+
+        while R <= j do
+            begin
+                tempArr[k] := arr[R];
+                R := R + 1;
+                k := k + 1;
+            end;
+
+        for k := i to j do
+            begin
+                arr[k] := tempArr[k];
+            end;
     end;  
 
-procedure IsSorted(var arr: array of Integer; n: Integer);
+procedure MergeSort(var arr: array of Integer; i, j: Integer);
+    var
+        m: integer;
     begin
+        if i < j then
+            begin
+                m := (i + j) div 2;  
+                MergeSort(arr, i, m);  
+                MergeSort(arr, m + 1, j);  
+                Merge(arr, i, j, m);  
+            end;
+    end;
 
-    end; }
+procedure PrintArray(Var arr: array of Integer; n: integer);
+    var
+        i: integer;
+    begin
+        write('Current Array: ');
+        writeln();
+        for i := 0 to n-1 do
+            begin
+                write(arr[i], ' ');
+                writeln();
+            end;
+    end;
+
+
+
+function IsSorted(var arr: array of Integer; i, n: Integer): Boolean;
+    begin
+        if i >= n - 1 then
+            begin
+                IsSorted := True;  
+                Exit;
+            end;
+
+        if arr[i] > arr[i + 1] then
+            begin
+                IsSorted := False;  
+                Exit;
+            end;
+
+        IsSorted := IsSorted(arr, i + 1, n);
+    end; 
+
 
 begin
     Test[0] := 10;
@@ -171,6 +254,21 @@ begin
     Test[2] := 3;
     Test[3] := 40;
     Test[4] := 1000;
-    QuickSortV2(Test,0,4);
-    writeln('Array values: ', Test[0], ', ', Test[1], ', ', Test[2], ', ', Test[3], ', ', Test[4]);
+    Test[5] := 50;
+    result := IsSorted(Test, 0, 6);
+    if result then
+        writeln('sorted')
+    else
+        begin
+            writeln('unsorted');
+        end;
+    MergeSort(Test,0,5);
+    result := IsSorted(Test, 0, 6);
+    if result then
+        writeln('sorted')
+    else
+        begin
+            writeln('unsorted');
+        end;
+    PrintArray(Test, 6);
 end.
